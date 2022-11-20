ac50f02009418b1c54fa8441657822be5930fca5
abdelhai95 <abdelhaijamal95@gamil.com>
2022-11-20 21:05:53 +0200

    content changed

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./*.{html,js}"],
+  content: ["./index.html", "./js/main.js"],
   theme: {
     screens: {
       sm: "640px",
